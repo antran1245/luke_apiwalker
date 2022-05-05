620e4cbffe6f79858588eb258cf0b2ef83ad8601
antran1245 <antran1245@gmail.com>
2022-05-04 18:34:41 -0700

    completed luke apiwalker

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,12 +1,42 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import obi from '../obi_wan_kenobi.jpg'
 export default function Info() {
     const {category , id} = useParams();
+    const [info, setInfo] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
     useEffect(()=>{
-        console.log(category)
+        setIsLoading(true);
+        axios.get(`https://swapi.dev/api/${category}/${id}`)
+        .then(response => {
+            setInfo(response.data)
+            setIsLoading(false)
+        })
+        .catch(err => {
+            setInfo(null)
+            setIsLoading(false)
+        })
     },[category, id])
 
     return(
-        <h1>hello {category}</h1>
+        <div>
+            {isLoading?
+                <p style={{margin:'0 10px'}}>Loading . . .</p>:
+                info?
+                    <div style={{margin:'0 10px'}}>
+                    {Object.keys(info).map(item => 
+                        <p key={item}>{item}: {info[item]}</p>
+                        )}
+                        </div>:
+                <div>
+                    <p>These aren't the droids you're looking for</p>
+                    <img src={obi} alt="obi" width={200} height={200}/>
+                </div>
+                
+            }
+
+        </div>
     );
 }
